047a593adf90b11925b4f05687faf08df3dbb77e
Matthew Born Jacobs <mjacobs4@berklee.edu>
2025-04-24 09:19:25 -0400

    level 2 complete

---
+++ level_two.py
@@ -0,0 +1,9 @@
+mix_of_stuff = [True, "wordswordswords", 42, 8.9]
+
+
+playlist_for_coding = ["Spitfire by Skrillex", "Homage by Mild High Club", "Topdown by Channel Tres", "The Dress by Dijon", "Breathe Deeper by Tame Impala"]
+
+playlist_for_coding.append("Time to Pretend by MGMT")
+
+
+print (playlist_for_coding[3])
